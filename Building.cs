2a20c24128bebe67d7501d02d056c0b91bd58c5a
robdan89 <fuquardwork@gmail.com>
2020-05-28 15:12:01 -0500

    initial commit

---
+++ Building.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace Planner
+{
+    public class Building
+    {
+        private string _designer;
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume
+        {
+            get
+            {
+                return Width * Depth * (3 * Stories);
+            }
+        }
+
+        public Building(string address)
+        {
+            _address = address;
+        }
+
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchase(string purchaserName)
+        {
+            _owner = purchaserName;
+        }
+
+        public void ProjectPlanner(string designer)
+        {
+            _designer = designer;
+
+            Console.WriteLine($"{_address}");
+            Console.WriteLine("---------------");
+            Console.WriteLine($"Desined by {_designer}");
+            Console.WriteLine($"Constructed on {_dateConstructed}");
+            Console.WriteLine($"Owned by {_owner}");
+            Console.WriteLine($"{Volume} cubic meters of space");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Planner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building OneElevenOcean = new Building("111 Ocean Blvd");
+            OneElevenOcean.Width = 10.5;
+            OneElevenOcean.Depth = 9.5;
+            OneElevenOcean.Stories = 15;
+            OneElevenOcean.Construct();
+            OneElevenOcean.Purchase("Daniel Fuqua");
+
+            Building SixtyTwoTwelve = new Building("6212 Springdale Dr");
+            SixtyTwoTwelve.Width = 20.32;
+            SixtyTwoTwelve.Depth = 15.65;
+            SixtyTwoTwelve.Stories = 30;
+            SixtyTwoTwelve.Construct();
+            SixtyTwoTwelve.Purchase("Billy Bob");
+
+            Building EightyEightyEightNewLine = new Building("8088 New Line Blvd");
+            EightyEightyEightNewLine.Width = 42.33;
+            EightyEightyEightNewLine.Depth = 20.11;
+            EightyEightyEightNewLine.Stories = 15;
+            EightyEightyEightNewLine.Construct();
+            EightyEightyEightNewLine.Purchase("Tom Arnold");
+
+            Building OneHundredOakley = new Building("100 Oakley St");
+            OneHundredOakley.Width = 16.87;
+            OneHundredOakley.Depth = 22.34;
+            OneHundredOakley.Stories = 40;
+            OneHundredOakley.Construct();
+            OneHundredOakley.Purchase("Big Daddy");
+
+
+
+            OneElevenOcean.ProjectPlanner("Bill Baker");
+            Console.WriteLine();
+            SixtyTwoTwelve.ProjectPlanner("Homer Simpson");
+            Console.WriteLine();
+            EightyEightyEightNewLine.ProjectPlanner("Peter Griffin");
+            Console.WriteLine();
+            OneHundredOakley.ProjectPlanner("Rick Sanchez");
+        }
+    }
+}
